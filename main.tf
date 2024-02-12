abbc03c33da8d2600ea3c861ed1c590515763c3f
divija231 <142985996+divija231@users.noreply.github.com>
2024-02-12 17:46:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,38 @@
 # main.tf
 
+resource "google_compute_network" "vpc_network" {
+  name                    = "vpc-network"
+  auto_create_subnetworks = false
+}
 
+resource "google_compute_subnetwork" "vpc_subnet" {
+  name          = "vpc-subnet"
+  ip_cidr_range = "10.2.0.0/24"
+  region        = "us-west1"
+  network       = google_compute_network.vpc_network.id
+}
+resource "google_compute_firewall" "rules" {
+  name        = "my-firewall-rule"
+  network     = "vpc-network"
+  description = "Creates firewall rule targeting tagged instances"
+
+  allow {
+    protocol  = "tcp"
+    ports     = ["80", "8080", "1000-2000","22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
 resource "google_container_cluster" "my_cluster" {
   name     = "my-gke-cluster"
   location = "us-west1"
+
+  initial_node_count = 1
+  network = google_compute_network.vpc_network.id
+  subnetwork = google_compute_subnetwork.vpc_subnet.id
+  default_max_pods_per_node =8
   deletion_protection = false
-  initial_node_count = 2
-  network = "default"
+  
+
   node_config {
     machine_type = "n1-standard-2"
     disk_size_gb = 75
